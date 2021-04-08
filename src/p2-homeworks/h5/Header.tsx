ac7f6343de242983980e368e8c29a1b16daa7352
romadrutskii <romadryuckii@gmail.com>
2021-04-08 14:47:24 +0300

    feat: move menu

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
-        </div>
+        <div className={s.headerWrapper}>
+            <button className={s.mainmenubtn}>Главное меню</button>
+            <ul className={s.nav}>
+                <li> <NavLink to="/pre-junior" activeClassName="selected">Home </NavLink></li>
+                <li><NavLink to="/pre-list" activeClassName="selected">List </NavLink></li>
+                <li><NavLink to="/pre-text-input" activeClassName="selected">Input </NavLink></li>
+                <li><NavLink to="/pre-button" activeClassName="selected">Button </NavLink></li>
+            </ul>
+            </div>
     )
 }
 
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Switch, Route, Redirect} from 'react-router-dom'
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
+import List from './pages/List';
+import Input from "./pages/Input";
+import Button from "./pages/Button";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
+    PRE_LIST:'/pre-list',
+    PRE_TEXT_INPUT:'/pre-text-input',
+    PRE_BUTTON:'/pre-button'
     // add paths
 }
 
@@ -9,19 +18,22 @@ function Routes() {
     return (
         <div>
             {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+            <Switch>
 
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
-            {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
+            <Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>
+            <Route path={PATH.PRE_LIST}  render={() => <List/>}/>
+            <Route path={PATH.PRE_TEXT_INPUT}  render={() => <Input/>}/>
+            <Route path={PATH.PRE_BUTTON} exact render={() => <Button/>}/>
 
-            {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
+            <Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>
                 // add routes
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+            <Route render={() => <Error404/>}/>
 
-            {/*</Switch>*/}
+            </Switch>
         </div>
     )
 }
