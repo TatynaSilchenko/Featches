f6da2ee07c2eb6ced0d07446eb9416458b14e6cb
romadrutskii <romadryuckii@gmail.com>
2021-04-20 12:34:29 +0300

    feat: reducers, sorted  object, filtered object

--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -3,27 +3,38 @@ import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
 type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 type SuperRadioPropsType = DefaultRadioPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+    name:string,
+    // options?: any,
+    options?: string[],
+    value:string,
+    onChangeOption?: (option: any) => void,
+    // type:any,
+
+
 }
 
 const SuperRadio: React.FC<SuperRadioPropsType> = (
     {
         type, name,
         options, value,
-        onChange, onChangeOption,
-        ...restProps
+       onChangeOption
     }
 ) => {
-    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // onChange, onChangeOption
+    const onChangeCallback = (e: any) => {
+        debugger
+        // onChange,
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
 
-    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
+    const mappedOptions: any[] = options ? options.map((o:any, i:any) => ( // map options with key
         <label key={name + '-' + i}>
             <input
                 type={'radio'}
+                name={name}
+                value={o}
+                onChange={onChangeCallback}
+                checked={o === value}
                 // name, checked, value, onChange
             />
             {o}
